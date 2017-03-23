b4908b9b0cd9df5bdd3ddc68ac5a51069c52fcb7
Sibylle de Courcel <sibylle44@gmail.com>
2017-03-23 14:11:22 +0100

    add ignore template to proxy

--- config.rb
+++ config.rb
@@ -31,7 +31,7 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 data.teachers.each do |member, _|
-  proxy "/teachers/#{member}.html", "/teachers/show.html", :locals => { :owner => member }
+  proxy "/teachers/#{member}.html", "/teachers/show.html", :locals => { :owner => member }, :ignore => true
 end
 ###
 # Helpers
